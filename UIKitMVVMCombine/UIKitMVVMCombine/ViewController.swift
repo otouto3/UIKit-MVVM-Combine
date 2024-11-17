bde4853423352c6e432ee5f38b12e2688780e8d1
otouto3 <otoutoyeah@gmail.com>
2024-11-17 17:35:11 +0900

    UIを配置

--- UIKitMVVMCombine/UIKitMVVMCombine/ViewController.swift
+++ UIKitMVVMCombine/UIKitMVVMCombine/ViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    private let idTextField = UITextField()
-    private let passwordTextField = UITextField()
+    private let idTextField = {
+        $0.borderStyle = .roundedRect
+        return $0
+    }(UITextField())
+    
+    private let passwordTextField = {
+        $0.borderStyle = .roundedRect
+        return $0
+    }(UITextField())
+    
     private let validationLabel: UILabel = {
         $0.font = .systemFont(ofSize: 18)
         return $0
     }(UILabel())
+    
     private lazy var stackView: UIStackView = {
         $0.axis = .vertical
         $0.spacing = 12
@@ -30,10 +39,15 @@ class ViewController: UIViewController {
     private func setup() {
         view.backgroundColor = .white
         view.addSubview(stackView)
+        view.addSubview(validationLabel)
          
         stackView.snp.makeConstraints {
-            $0.top.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(16)
             $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        validationLabel.snp.makeConstraints {
+            $0.top.equalTo(stackView).offset(16)
         }
     }
 }
