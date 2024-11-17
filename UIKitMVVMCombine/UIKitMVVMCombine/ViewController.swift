8a9b3b6388bd783508741cb81ac027c5e55a6cee
otouto3 <otoutoyeah@gmail.com>
2024-11-17 17:27:44 +0900

    wip

--- UIKitMVVMCombine/UIKitMVVMCombine/ViewController.swift
+++ UIKitMVVMCombine/UIKitMVVMCombine/ViewController.swift
@@ -6,13 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    private let idTextField = UITextField()
+    private let passwordTextField = UITextField()
+    private let validationLabel: UILabel = {
+        $0.font = .systemFont(ofSize: 18)
+        return $0
+    }(UILabel())
+    private lazy var stackView: UIStackView = {
+        $0.axis = .vertical
+        $0.spacing = 12
+        return $0
+    }(UIStackView(arrangedSubviews: [idTextField, passwordTextField]))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .cyan
+        
+        setup()
+    }
+    
+    private func setup() {
+        view.backgroundColor = .white
+        view.addSubview(stackView)
+         
+        stackView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
     }
 }
 
---
+++ UIKitMVVMCombine/UIKitMVVMCombine/ViewModelswift.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelswift.swift
+//  UIKitMVVMCombine
+//
+//  Created by ごつ on 2024/11/17.
+//
+
+import Foundation
